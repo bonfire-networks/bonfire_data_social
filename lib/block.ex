7705953410f41477ca134aa184c78a4279ad167a
jjl <james.laver@gmail.com>
2020-11-28 13:16:56 +0000

    add block and bookmark

---
+++ lib/block.ex
@@ -0,0 +1,112 @@
+defmodule Bonfire.Data.Social.Block do
+
+  use Pointers.Pointable,
+    otp_app: :bonfire_data_social,
+    table_id: "B10CK1NGSTVFFAV01DSSEE1NG1",
+    source: "bonfire_data_social_block"
+
+  require Pointers.Changesets
+  alias Bonfire.Data.Social.Block
+  alias Pointers.{Changesets, Pointer}
+  
+  pointable_schema do
+    belongs_to :blocker, Pointer
+    belongs_to :blocked, Pointer
+  end
+
+  @defaults [
+    cast:     [:blocker_id, :blocked_id],
+    required: [:blocker_id, :blocked_id],
+  ]
+  def changeset(block \\ %Block{}, attrs, opts \\ []),
+    do: Changesets.auto(block, attrs, opts, @defaults)
+
+end
+defmodule Bonfire.Data.Social.Block.Migration do
+
+  import Ecto.Migration
+  import Pointers.Migration
+  alias Bonfire.Data.Social.Block
+
+  @block_table Block.__schema__(:source)
+  @unique_index [:blocker_id, :blocked_id]
+
+  # create_block_table/{0,1}
+
+  defp make_block_table(exprs) do
+    quote do
+      require Pointers.Migration
+      Pointers.Migration.create_pointable_table(Bonfire.Data.Social.Block) do
+        Ecto.Migration.add :blocker_id, strong_pointer(), null: false
+        Ecto.Migration.add :blocked_id, strong_pointer(), null: false
+        unquote_splicing(exprs)
+      end
+    end
+  end
+
+  defmacro create_block_table(), do: make_block_table([])
+  defmacro create_block_table([do: {_, _, body}]), do: make_block_table(body)
+
+  # drop_block_table/0
+
+  def drop_block_table(), do: drop_pointable_table(Block)
+
+  # create_block_unique_index/{0,1}
+
+  defp make_block_unique_index(opts) do
+    quote do
+      Ecto.Migration.create_if_not_exists(
+        Ecto.Migration.unique_index(unquote(@block_table), unquote(@unique_index), unquote(opts))
+      )
+    end
+  end
+
+  defmacro create_block_unique_index(opts \\ [])
+  defmacro create_block_unique_index(opts), do: make_block_unique_index(opts)
+
+  def drop_block_unique_index(opts \\ [])
+  def drop_block_unique_index(opts), do: drop_if_exists(unique_index(@block_table, @unique_index, opts))
+
+  defp make_block_blocked_index(opts) do
+    quote do
+      Ecto.Migration.create_if_not_exists(
+        Ecto.Migration.index(unquote(@block_table), [:blocked_id], unquote(opts))
+      )
+    end
+  end
+
+  defmacro create_block_blocked_index(opts \\ [])
+  defmacro create_block_blocked_index(opts), do: make_block_blocked_index(opts)
+
+  def drop_block_blocked_index(opts \\ []) do
+      drop_if_exists(index(@block_table, [:blocked_id], opts))
+  end
+
+  # migrate_block/{0,1}
+
+  defp mf(:up) do
+    quote do
+      unquote(make_block_table([]))
+      unquote(make_block_unique_index([]))
+      unquote(make_block_blocked_index([]))
+    end
+  end
+  defp mf(:down) do
+    quote do
+      Bonfire.Data.Social.Block.Migration.drop_block_blocked_index()
+      Bonfire.Data.Social.Block.Migration.drop_block_unique_index()
+      Bonfire.Data.Social.Block.Migration.drop_block_table()
+    end
+  end
+
+  defmacro migrate_block() do
+    quote do
+      if Ecto.Migration.direction() == :up,
+        do: unquote(mf(:up)),
+        else: unquote(mf(:down))
+    end
+  end
+
+  defmacro migrate_block(dir), do: mf(dir)
+
+end
---
+++ lib/bookmark.ex
@@ -0,0 +1,112 @@
+defmodule Bonfire.Data.Social.Bookmark do
+
+  use Pointers.Pointable,
+    otp_app: :bonfire_data_social,
+    table_id: "B10CK1NGSTVFFAV01DSSEE1NG1",
+    source: "bonfire_data_social_bookmark"
+
+  require Pointers.Changesets
+  alias Bonfire.Data.Social.Bookmark
+  alias Pointers.{Changesets, Pointer}
+  
+  pointable_schema do
+    belongs_to :bookmarker, Pointer
+    belongs_to :bookmarked, Pointer
+  end
+
+  @defaults [
+    cast:     [:bookmarker_id, :bookmarked_id],
+    required: [:bookmarker_id, :bookmarked_id],
+  ]
+  def changeset(bookmark \\ %Bookmark{}, attrs, opts \\ []),
+    do: Changesets.auto(bookmark, attrs, opts, @defaults)
+
+end
+defmodule Bonfire.Data.Social.Bookmark.Migration do
+
+  import Ecto.Migration
+  import Pointers.Migration
+  alias Bonfire.Data.Social.Bookmark
+
+  @bookmark_table Bookmark.__schema__(:source)
+
+  # create_bookmark_table/{0,1}
+
+  defp make_bookmark_table(exprs) do
+    quote do
+      require Pointers.Migration
+      Pointers.Migration.create_pointable_table(Bonfire.Data.Social.Bookmark) do
+        Ecto.Migration.add :bookmarker_id, strong_pointer(), null: false
+        Ecto.Migration.add :bookmarked_id, strong_pointer(), null: false
+        unquote_splicing(exprs)
+      end
+    end
+  end
+
+  defmacro create_bookmark_table(), do: make_bookmark_table([])
+  defmacro create_bookmark_table([do: {_, _, body}]), do: make_bookmark_table(body)
+
+  # drop_bookmark_table/0
+
+  def drop_bookmark_table(), do: drop_pointable_table(Bookmark)
+
+  # create_bookmark_bookmarker_index/{0,1}
+
+  defp make_bookmark_bookmarker_index(opts) do
+    quote do
+      Ecto.Migration.create_if_not_exists(
+        Ecto.Migration.index(unquote(@bookmark_table), [:bookmarker_id], unquote(opts))
+      )
+    end
+  end
+
+  defmacro create_bookmark_bookmarker_index(opts \\ [])
+  defmacro create_bookmark_bookmarker_index(opts), do: make_bookmark_bookmarker_index(opts)
+
+  def drop_bookmark_bookmarker_index(opts \\ [])
+  def drop_bookmark_bookmarker_index(opts),
+    do: drop_if_exists(index(@bookmark_table, [:bookmarker_id], opts))
+
+  defp make_bookmark_bookmarked_index(opts) do
+    quote do
+      Ecto.Migration.create_if_not_exists(
+        Ecto.Migration.index(unquote(@bookmark_table), [:bookmarked_id], unquote(opts))
+      )
+    end
+  end
+
+  defmacro create_bookmark_bookmarked_index(opts \\ [])
+  defmacro create_bookmark_bookmarked_index(opts), do: make_bookmark_bookmarked_index(opts)
+
+  def drop_bookmark_bookmarked_index(opts \\ []) do
+      drop_if_exists(index(@bookmark_table, [:bookmarked_id], opts))
+  end
+
+  # migrate_bookmark/{0,1}
+
+  defp mb(:up) do
+    quote do
+      unquote(make_bookmark_table([]))
+      unquote(make_bookmark_bookmarker_index([]))
+      unquote(make_bookmark_bookmarked_index([]))
+    end
+  end
+  defp mb(:down) do
+    quote do
+      Bonfire.Data.Social.Bookmark.Migration.drop_bookmark_bookmarked_index()
+      Bonfire.Data.Social.Bookmark.Migration.drop_bookmark_bookmarker_index()
+      Bonfire.Data.Social.Bookmark.Migration.drop_bookmark_table()
+    end
+  end
+
+  defmacro migrate_bookmark() do
+    quote do
+      if Ecto.Migration.direction() == :up,
+        do: unquote(mb(:up)),
+        else: unquote(mb(:down))
+    end
+  end
+
+  defmacro migrate_bookmark(dir), do: mb(dir)
+
+end
