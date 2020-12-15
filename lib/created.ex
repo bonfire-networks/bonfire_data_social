a2f141fe3a6ba54bab0bb1dfea6d5572e888a3d4
Karen Konou <konoukaren@gmail.com>
2020-12-15 11:16:18 +0100

    Add created table

---
+++ lib/created.ex
@@ -0,0 +1,66 @@
+defmodule Bonfire.Data.Social.Created do
+  use Pointers.Mixin,
+    otp_app: :bonfire_data_social,
+    source: "bonfire_data_social_created"
+
+  alias Bonfire.Data.Social.Created
+  alias Pointers.{Changesets, Pointer}
+
+  mixin_schema do
+    belongs_to :creator, Pointer
+  end
+
+  @defaults [
+    cast: [:creator_id],
+    required: [:creator_id]
+  ]
+
+  def changeset(created \\ %Created{}, attrs, opts \\ []),
+    do: Changesets.auto(created, attrs, opts, @defaults)
+end
+
+defmodule Bonfire.Data.Social.Created.Migration do
+  use Ecto.Migration
+  import Pointers.Migration
+  alias Bonfire.Data.Social.Created
+
+  # create_created_table/{0, 1}
+
+  defp make_created_table(exprs) do
+    quote do
+      require Pointers.Migration
+      Pointers.Migration.create_mixin_table(Bonfire.Data.Social.Created) do
+        Ecto.Migration.add :creator_id, Pointers.Migration.strong_pointer()
+        unquote_splicing(exprs)
+      end
+    end
+  end
+
+  defmacro create_created_table(), do: make_created_table([])
+  defmacro create_created_table([do: {_, _, body}]), do: make_created_table(body)
+
+  # drop_created_table/0
+
+  def drop_created_table(), do: drop_mixin_table(Created)
+
+  # migrate_created/{0, 1}
+
+  defp mcd(:up), do: make_created_table([])
+
+  defp mcd(:down) do
+    quote do
+      Bonfire.Data.Social.Created.Migration.drop_created_table()
+    end
+  end
+
+  defmacro migrate_created() do
+    quote do
+      if Ecto.Migration.direction() == :up,
+        do: unquote(mcd(:up)),
+        else: unquote(mcd(:down))
+    end
+  end
+
+  defmacro migrate_created(dir), do: mcd(dir)
+
+end
